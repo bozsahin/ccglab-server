e4aa947198da2ddeb11b9c5a14ee760fe3a4943e
bozsahin <cem.bozsahin@gmail.com>
2019-03-29 09:52:27 +0300

    starting to build the server

---
+++ ccglab-serve.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+HTTPS = 443
+PORT = HTTPS # only https for security freaks at uni's computer center
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
