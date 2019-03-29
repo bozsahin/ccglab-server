f8c4e52eda7a181bb645b9c63e1eed35c776ad13
Cem Bozsahin <bozsahin@users.noreply.github.com>
2019-03-29 09:55:24 +0300

    Update ccglab-serve.py

--- ccglab-serve.py
+++ ccglab-serve.py
@@ -9,5 +9,5 @@ PORT = HTTPS # only https for security freaks at uni's computer center
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    print("CCGlab at your service through port", PORT)
     httpd.serve_forever()
