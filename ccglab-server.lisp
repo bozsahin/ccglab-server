dc51e463fb28a8c80b4da02b0d80944786ea2fdf
bozsahin <cem.bozsahin@gmail.com>
2019-05-25 08:19:26 +0300

    server loads ccglab from api

--- ccglab-server.lisp
+++ ccglab-server.lisp
@@ -1,36 +1,43 @@
 ;; CCGlab web server using SBCL, quicklisp and Portabe AllegroServe
 ;; -- cem bozsahin, 2019
 
+(load #p"/Users/bozsahin/myrepos/ccglab-api/ccglab-sbcl.lisp") ; loads CCGlab as API, creates :ccglab package and returns to :cl
+
 (load #p"/Users/bozsahin/quicklisp/setup.lisp") ; easiest way to fetch aserve
 (ql:quickload :aserve) ; loads portable AllegroServe, which creates the package :net.aserve
 
-(defpackage :ccglab-server (:use :cl :net.aserve :sb-ext)) ; own package with everything imported
+(defpackage :ccglab-server (:use :cl :net.aserve :sb-ext :ccglab)) ; own package with everything imported
 
 (in-package :ccglab-server)
 
 (defparameter *ccglab-port* 8000)
-(defparameter *l-main* "/Users/bozsahin/myrepos/ccglab-server/main.html") ; local main
-(defparameter *w-main* "/main.html")                                     ; web main
-(defparameter *l-exampleg* "/Users/bozsahin/myrepos/ccglab-server/wish.ccg") 
-(defparameter *w-exampleg* "/wish.ccg")  
-(defparameter *l-icon* "/Users/bozsahin/myrepos/ccglab-server/ccglab2.ico") 
-(defparameter *w-icon* "/ccglab2.ico")  
-(defparameter *l-manual* "/Users/bozsahin/myrepos/ccglab-server/CCGlab-manual.pdf") 
-(defparameter *w-manual* "/CCGlab-manual.pdf")  
-(defparameter *l-display* "/Users/bozsahin/myrepos/ccglab-server/wish-framed.jpg") 
-(defparameter *w-display* "/wish-framed.jpg")  
-(defparameter *static-pages* (list (list *l-main* *w-main*) 
-				   (list *l-exampleg* *w-exampleg*)
-				   (list *l-icon* *w-icon*)
-				   (list *l-manual* *w-manual*)
-				   (list *l-display* *w-display*)))
-(format t "~%Killing listeners if I have to for port: ~A" *ccglab-port*)
+;; static files - list of local and public file names
+(defparameter *main* (list "/Users/bozsahin/myrepos/ccglab-server/main.html"
+			   "/main.html"))
+(defparameter *exampleg* (list "/Users/bozsahin/myrepos/ccglab-server/wish.ccg"
+			       "/wish.ccg"))
+(defparameter *icon* (list "/Users/bozsahin/myrepos/ccglab-server/ccglab2.ico"
+			   "/ccglab2.ico"))  
+(defparameter *manual* (list "/Users/bozsahin/myrepos/ccglab-server/CCGlab-manual.pdf"
+			     "/CCGlab-manual.pdf"))
+(defparameter *display* (list "/Users/bozsahin/myrepos/ccglab-server/wish-framed.jpg"
+			      "/wish-framed.jpg"))
+(defparameter *static-pages* (list *main* *exampleg* *icon* *manual* *display*))
+
+(defmacro get-local (p)
+  `(first ,p))
+
+(defmacro get-public (p)
+  `(second ,p))
+
+;;(format t "======================================")
+(format t "~%Attempting to kill listeners to port: ~A~%" *ccglab-port*)
 (run-program "kill-ccglab-port" (list (write-to-string *ccglab-port*)) 
-	     :search t :wait t) ; from sb-ext
-(format t "~%Starting CCGlab web server at port: ~A" *ccglab-port*)
+	     :output *standard-output* :search t :wait t) ; from sb-ext
+(format t "Starting CCGlab web server at port: ~A" *ccglab-port*)
 (start :port *ccglab-port*)
 (defun publish-statics ()
   (dolist (fpair *static-pages*)
-    (publish-file :path  (second fpair) :file (first fpair))))
+    (publish-file :path  (get-public fpair) :file (get-local fpair))))
 (publish-statics)
 
